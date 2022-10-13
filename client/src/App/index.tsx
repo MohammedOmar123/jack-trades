1bd4a41af90083b47f0cda7c2abeddf89e055b8e
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-13 12:22:06 +0300

    delete mui package from server's json file and install it in the client's package json, and add test button relates #34

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from '@mui/material/Button';
 
 import "./style.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return <div className="App">
+   <Button variant="contained">Hello World</Button>;
+
+  </div>;
 }
 
 export default App;
