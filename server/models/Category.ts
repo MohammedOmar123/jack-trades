25c43a03e9c4794131cc711aefb60d6f6505add7
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-16 14:42:13 +0300

    change category to Category and product to Product relates #55

--- server/models/Category.ts
+++ server/models/Category.ts
@@ -1,18 +1,19 @@
-import  DataTypes from 'sequelize';
+import DataTypes from 'sequelize';
 import sequelize from '../database/connection';
- const Category = sequelize.define('category', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-    },
-    image: {
-        type: DataTypes.TEXT,
-        allowNull:false,
-    }
+
+const Category = sequelize.define('Category', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
 });
-export default Category ;
+export default Category;
--- server/models/Prouduct.ts
+++ server/models/Prouduct.ts
@@ -1,42 +1,34 @@
-import  DataTypes  from 'sequelize';
+import DataTypes from 'sequelize';
 import sequelize from '../database/connection';
 
-import  Category  from './Category';
-import User from './users';
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  gallery: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: false,
+  },
+  is_available: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.ENUM('donation', 'exchange'),
+    allowNull: false,
+  },
 
- const Product = sequelize.define('product', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    title: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-    gallery: {
-        type: DataTypes.ARRAY(DataTypes.STRING),
-        allowNull: false,
-    },
-    isAvailable: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-        allowNull: false,
-    },
-    type: {
-        type: DataTypes.ENUM('donation', 'exchange'),
-        allowNull: false,
-    }
-
-})
-User.hasMany(Product,{foreignKey:'user_id'});
-Product.belongsTo(User);
-
-Category.hasMany(Product,{foreignKey:'category_id'});
-Product.belongsTo(Category);
+});
 
 export default Product;
