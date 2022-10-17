3d6b908a44ecc9058355c7f98138112328b57d23
unknown <saritadh74@gmail.com>
2022-10-17 15:35:00 +0300

    defined types for the static data relates #37

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -1,1 +1,5 @@
-export {};
+import { IntroTypes } from './introDataTypes';
+
+export {
+  IntroTypes,
+};
---
+++ client/src/interfaces/introDataTypes.ts
@@ -0,0 +1,6 @@
+export interface IntroTypes {
+  icon: string,
+  title: string,
+  description: string,
+  color: string
+}
