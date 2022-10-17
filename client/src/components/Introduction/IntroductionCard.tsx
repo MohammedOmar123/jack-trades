e0991d00f2880d173a75ba6838db3e0734282292
unknown <saritadh74@gmail.com>
2022-10-17 16:49:47 +0300

    used materialui typography relates #37

--- client/src/components/Introduction/IntroductionCard.tsx
+++ client/src/components/Introduction/IntroductionCard.tsx
@@ -1,17 +1,18 @@
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC } from 'react';
+import { Typography } from '@mui/material';
 import { IntroTypes } from '../../interfaces';
 
 const IntroductionCard:FC< { data: IntroTypes } > = ({ data }) => (
   <div className="introCard">
     <div>
       <i className={data.icon} style={{ color: data.color }} />
-      <h3>{data.title}</h3>
+      <Typography variant="h5">{data.title}</Typography>
     </div>
-    <p>
+    <Typography>
       {data.description}
-    </p>
+    </Typography>
   </div>
 );
 
