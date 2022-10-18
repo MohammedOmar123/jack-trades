35bcc1fb43b2c545d2dbb7111ee42753f718fa3b
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2022-10-18 12:46:46 +0300

    move the router from the app to the index and change the router to have childrens

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,8 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import './style.css';
-import { router, RouterProvider } from './router';
 
 const App = () => (
-  <RouterProvider router={router} />
+  <div>
+    <h1>Nav bar</h1>
+    <Outlet />
+    <h2>Footer</h2>
+  </div>
 );
 
 export default App;
