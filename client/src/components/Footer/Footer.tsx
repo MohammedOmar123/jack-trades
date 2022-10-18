12012cd2e2c111287c1dbd5dbd6e173eadf7e046
Hakim <abdalhakem1226@gmail.com>
2022-10-18 13:40:33 +0300

    converting component to material ui relates #33

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,60 +1,96 @@
-/* eslint-disable max-len */
+import {
+  Grid, Typography, List, ListItem, ListItemText, Link,
+} from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import './Footer.css';
 
 const Footer = () => (
-  <div id="footer">
-    <div className="shopByCategory">
-      <h3>Shop By Category</h3>
-      <ul className="cateogiesList">
-        <li>Clothes</li>
-        <li>Furnature</li>
-        <li>Accessories</li>
-        <li>Electonics</li>
-        <li>Books</li>
-      </ul>
-    </div>
-    <div className="about">
-      <h3>About</h3>
-      <ul>
-        <li>Contact Us</li>
-        <li>About Us</li>
-      </ul>
-    </div>
-    <div className="policy">
-      <h3>Policy</h3>
-      <ul>
-        <li>Retrun Policy</li>
-        <li>Terms of Use</li>
-        <li>Security</li>
-        <li>Privacy</li>
-      </ul>
-    </div>
-    <div className="teamMembers">
-      <div className="teamMembersContainer">
-        <h3>Founders</h3>
-        <ul>
-          <li className="teamMemberDetails">
-            <a href="https://github.com/hkmusameh01" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-github" />
-            </a>
-            <p className="name">Abdalhakim Abumusameh</p>
-          </li>
-          <li className="teamMemberDetails">
-            <a href="https://github.com/MohammedOmar123" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-github" />
-            </a>
-            <p className="name">Sara Dahman</p>
-          </li>
-          <li className="teamMemberDetails">
-            <a href="https://github.com/SaraDahman" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-github" />
-            </a>
-            <p className="name">Mohammed Balousha</p>
-          </li>
-        </ul>
+  <Grid
+    id="footer"
+    container
+    padding={5}
+    height={320}
+  >
+    <Grid item md={2.5}>
+      <div>
+        <Typography variant="h6">Shop By Category</Typography>
+        <List>
+          <ListItem disablePadding>
+            <ListItemText primary="Clothes" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Furnature" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Accessories" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Electonics" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Books" />
+          </ListItem>
+        </List>
       </div>
-    </div>
-  </div>
+    </Grid>
+    <Grid item md={2.5}>
+      <div>
+        <Typography variant="h6">About</Typography>
+        <List>
+          <ListItem disablePadding>
+            <ListItemText primary="About Us" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Contact Us" />
+          </ListItem>
+        </List>
+      </div>
+    </Grid>
+    <Grid item md={2.5}>
+      <div>
+        <Typography variant="h6">Policy</Typography>
+        <List>
+          <ListItem disablePadding>
+            <ListItemText primary="Retrun Policy" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Terms of Use" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Security" />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Privacy" />
+          </ListItem>
+        </List>
+      </div>
+    </Grid>
+    <Grid item md>
+      <Typography variant="h6">Founders</Typography>
+      <List>
+        <ListItem disablePadding>
+          <Link href="https://github.com/hkmusameh01" color="inherit">
+            <GitHubIcon fontSize="large" />
+          </Link>
+          <ListItemText primary="Abdalhakim Abumusameh" />
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href="https://github.com/hkmusameh01" color="inherit">
+            <GitHubIcon fontSize="large" />
+          </Link>
+
+          <ListItemText primary="Sara Dahman" />
+        </ListItem>
+        <ListItem disablePadding>
+          <Link href="https://github.com/hkmusameh01" color="inherit">
+            <GitHubIcon fontSize="large" />
+          </Link>
+
+          <ListItemText primary="Mohammed Balousha" />
+        </ListItem>
+      </List>
+    </Grid>
+  </Grid>
 );
 
 export default Footer;
