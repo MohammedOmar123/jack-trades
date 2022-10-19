2be7b1cc7ef31d5706cf8d0cb0cbf8307a7f6e91
Hakim <abdalhakem1226@gmail.com>
2022-10-19 21:55:28 +0300

    complete 90% of the style with mui relates #39

--- client/src/components/Statistics/Statistics.tsx
+++ client/src/components/Statistics/Statistics.tsx
@@ -1,31 +1,70 @@
 /* eslint-disable max-len */
 import {
-  Box, List, ListItem, ListItemText,
+  Box, Container, Typography,
 } from '@mui/material';
 
-const Statistics = () => (
-  <Box sx={{
-    width: '100%',
-    height: '20rem',
-    backgroundColor: '#3388b8',
-  }}
-  >
-    <List>
-      <ListItem sx={{
-        border: '1px solid',
-        display: 'flex',
-        justifyContent: 'space-around',
-        textAlign: 'center',
-        gap: '5rem',
-        padding: '2rem 6rem',
-      }}
-      >
-        <ListItemText sx={{ border: '1px solid', width: '5rem' }} primary="hello" />
-        <ListItemText sx={{ border: '1px solid' }} primary="hello" />
-        <ListItemText sx={{ border: '1px solid' }} primary="hello" />
-      </ListItem>
-    </List>
+import './Statistics.css';
 
+const containerStyle = {
+  // outline: '1px solid',
+  width: '25rem',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const typoStyle = {
+  letterSpacing: '.1rem',
+};
+
+const Statistics = () => (
+  <Box
+    bgcolor="#3388b8"
+    height="15rem"
+    display="flex"
+    pr="8rem"
+    pl="8rem"
+  >
+    <Container sx={containerStyle} disableGutters>
+      <Typography variant="h3" fontWeight="bolder">70</Typography>
+      <Typography
+        sx={typoStyle}
+        variant="subtitle2"
+        fontSize="1.3rem"
+        fontWeight="bold"
+        mt=".5rem"
+      >
+        Contributions
+
+      </Typography>
+    </Container>
+    <Container sx={containerStyle} disableGutters>
+      <Typography variant="h3" fontWeight="bolder">20</Typography>
+      <Typography
+        sx={typoStyle}
+        variant="subtitle2"
+        fontSize="1.3rem"
+        fontWeight="bold"
+        mt=".5rem"
+      >
+        Exchanges
+
+      </Typography>
+    </Container>
+    <Container sx={containerStyle} disableGutters>
+      <Typography variant="h3" fontWeight="bolder">50</Typography>
+      <Typography
+        sx={typoStyle}
+        variant="subtitle2"
+        fontSize="1.3rem"
+        fontWeight="bold"
+        mt=".5rem"
+      >
+        Donations
+
+      </Typography>
+    </Container>
   </Box>
 );
 
