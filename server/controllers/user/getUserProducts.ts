3d697ec35a38e470eddd573e35d0468685b1aebd
unknown <saritadh74@gmail.com>
2022-10-19 13:59:29 +0300

    created the controller which responds with the user products relates #22

--- server/controllers/user/getUserProducts.ts
+++ server/controllers/user/getUserProducts.ts
@@ -1,6 +1,15 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { getUserProductsQuery } from '../../database/queries';
+import { CustomError } from '../../helpers';
 
-const getUserProducts = (req : Request, res : Response) => {
-  res.send('Hello from user products');
+const getUserProducts = async (req : Request, res : Response, next:NextFunction) => {
+  const { userId } = req.params;
+  try {
+    if (!(Number(userId) > 0)) throw new CustomError(401, 'Bad Request');
+    const data = await getUserProductsQuery(userId);
+    res.status(200).send({ data });
+  } catch (err) {
+    next(err);
+  }
 };
 export default getUserProducts;
