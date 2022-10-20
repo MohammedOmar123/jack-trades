65be946b884bee6e763953f02c9815a78e9f2624
unknown <saritadh74@gmail.com>
2022-10-20 16:30:46 +0300

    finished the user info component relates #48

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Box, Typography } from '@mui/material';
+import { UserInfo } from '../components';
+
 const ProfilePage = () => (
-  <div>ProfilePage</div>
+  <Box className="user-profile">
+    <UserInfo />
+  </Box>
 );
 
 export default ProfilePage;
