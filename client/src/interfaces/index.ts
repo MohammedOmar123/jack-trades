1c41576805c4ee80404811a50d1139704ba84c10
unknown <saritadh74@gmail.com>
2022-10-20 11:02:26 +0300

    defined the data types for the categories array of data relates #38

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -1,9 +1,11 @@
-import { IntroTypes } from './introDataTypes';
+import IntroTypes from './introDataTypes';
 import ButtonPropsTypes from './ButtonPropsTypes';
 import ImagePropsTypes from './ImagePropsTypes';
+import CategoriesTypes from './CategoriesPropsTypes';
 
 export {
   IntroTypes,
   ButtonPropsTypes,
   ImagePropsTypes,
+  CategoriesTypes,
 };
