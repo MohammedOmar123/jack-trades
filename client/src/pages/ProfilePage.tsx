9756095b3934767c696f215e4d7cd5ba7b598a96
unknown <saritadh74@gmail.com>
2022-10-23 00:12:22 +0300

    fetched the data for the user profile and passed it as props relates #49

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,11 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Box, Typography } from '@mui/material';
-import { UserInfo } from '../components';
+import { FC, useState, useEffect } from 'react';
+import { Box } from '@mui/material';
+import axios from 'axios';
+import { UserInfo, UserProducts } from '../components';
+import { UserProduct, UserInfoTypes } from '../interfaces';
 
-const ProfilePage = () => (
-  <Box className="user-profile">
-    <UserInfo />
-  </Box>
-);
+const ProfilePage:FC = () => {
+  const [products, setProducts] = useState< UserProduct[] | null>(null);
+  const [info, setInfo] = useState<UserInfoTypes | null >(null);
+
+  useEffect(() => {
+    (async () => {
+      const userProducts = await axios.get('/api/v1/user/1/products');
+      const userInfo = await axios.get('/api/v1/user/1');
+
+      setProducts(userProducts.data);
+      setInfo(userInfo.data);
+    })();
+  }, []);
+
+  if (!products || !info) return <h1>loading ...</h1>;
+  return (
+    <Box className="user-profile">
+      <UserInfo info={info} />
+      <UserProducts products={products} />
+    </Box>
+  );
+};
 
 export default ProfilePage;
