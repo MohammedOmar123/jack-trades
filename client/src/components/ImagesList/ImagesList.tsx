9c5a4af8fa7ddb6abc8d206fe889003d0634cd1b
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-22 19:58:41 +0300

    delete key props and use React.Children.toArray function and rename con to context

--- client/src/components/ImagesList/ImagesList.tsx
+++ client/src/components/ImagesList/ImagesList.tsx
@@ -1,23 +1,25 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import { Box } from '@mui/material';
 import Image from '../Image/Image';
 import './ImageList.css';
 import { ImageContext } from '../Context/ImageContext';
 
 const ImagesList = () => {
-  const con = useContext(ImageContext);
+  const context = useContext(ImageContext);
   return (
     <Box className="ImagesContainer">
       {
-    con.gallery.map((image) => (
-      <Image
-        key={Math.random() + Date.now()}
-        attributes={{
-          src: image,
-          alt: 'Product Image',
-          className: 'images-gallery',
-        }}
-      />
+    context.gallery.map((image) => (
+      React.Children.toArray(
+        <Image
+          attributes={{
+            src: image,
+            alt: 'Product Image',
+            className: 'images-gallery',
+          }}
+        />,
+      )
+
     ))
 }
     </Box>
