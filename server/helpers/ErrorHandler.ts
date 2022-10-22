990d6ab32cc413b8efa9f2cad4464e81f4ac16ef
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-22 17:59:47 +0300

    handle validation Errors responses

--- server/helpers/ErrorHandler.ts
+++ server/helpers/ErrorHandler.ts
@@ -3,6 +3,8 @@ import { Response, Request, NextFunction } from 'express';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ErrorHandler = (error, req: Request, res: Response, next: NextFunction) => {
   const { status, message } = error;
+
+  if (error.name === 'ValidationError') return res.status(400).json({ msg: error.message });
   res.status(status || 500).send({ msg: message || 'Internal Server Error' });
 };
 export default ErrorHandler;
