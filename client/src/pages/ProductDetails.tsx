fa60c3a5b2db075f9d8916f329e9aa551cd5e943
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-22 03:51:41 +0300

    Got the data from the api  , also Handling loading and error cases relates #84

--- client/src/pages/ProductDetails.tsx
+++ client/src/pages/ProductDetails.tsx
@@ -1,5 +1,44 @@
-const ProductDetails = () => (
-  <div>ProductDetails</div>
-);
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { ProductContainer } from '../components';
+import { IProduct } from '../interfaces';
+import { ImageContextProvider } from '../components/Context/ImageContext';
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState<IProduct | null>(null);
+  const [error, setError] = useState<boolean | string>(false);
+  useEffect(() => {
+    axios.get(`/api/v1/products/${id}`)
+      .then((response) => {
+        setProduct(response.data);
+      }).catch(() => {
+        setError(`You're all good, it's out bad.
+We're having some errors in getting the information. We're working on it.`);
+      });
+  }, []);
+  if (error) {
+    return (
+      <h4 style={{ margin: '260px' }}>
+        {error}
+      </h4>
+    );
+  }
+  if (product === null) {
+    return (
+      <h5 style={{ margin: '200px' }}>
+        Loading ..
+      </h5>
+    );
+  }
+  return (
+    <div>
+      <ImageContextProvider gallery={product.gallery}>
+        <ProductContainer attributes={product} />
+      </ImageContextProvider>
+    </div>
+  );
+};
 
 export default ProductDetails;
