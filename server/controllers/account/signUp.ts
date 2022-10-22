8d286825bea6eee57ce3472837bfbd216c889371
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-22 19:18:12 +0300

    import generateToken Function  to create a token for users when they signup

--- server/controllers/account/signUp.ts
+++ server/controllers/account/signUp.ts
@@ -1,20 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 import bcrypt from 'bcrypt';
-import validateSighup from '../../helpers/signupValidation';
+import { validateSighup } from '../../validation';
 import { signupQuery, checkUserExist } from '../../database/queries/account';
 import { CustomError } from '../../helpers';
+import generateToken from '../../helpers/generateToken';
 
-const signup = async (req : Request, res : Response, next:NextFunction) => {
+const signup = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { id, email, password } = req.body;
+    const { email, password } = req.body;
     await validateSighup(req.body);
+
     const checkResultQuery = await checkUserExist(email);
 
-    if (checkResultQuery) throw new CustomError(400, 'This email is already exist,Please check your email again ');
-    const hashed = await bcrypt.hash(password, 10);
+    if (checkResultQuery) throw new CustomError(400, 'This email is already exist,Please check your email again');
 
-    await signupQuery({ ...req.body, password: hashed });
-    res.cookie('token', { id, email }).json({ msg: 'Your Account Created Successfully' });
+    const hashed = await bcrypt.hash(password, 10);
+    const result = await signupQuery({ ...req.body, password: hashed });
+
+    const token = await generateToken(result.getDataValue('id'), email);
+
+    res.cookie('token', token, { httpOnly: true, secure: true }).json({ msg: 'Your Account Created Successfully' });
   } catch (err) {
     next(err);
   }
