f42872b7d41ac52ab8c3c215e03e7ede5163da1f
Hakim <abdalhakem1226@gmail.com>
2022-10-23 16:16:14 +0300

    add statistics section to appear at the page relates #39

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar, Footer } from '../components';
+import Statistics from '../components/Statistics/Statistics';
 import './style.css';
 
 const App = () => (
   <div>
     <Navbar />
     <Outlet />
+    <Statistics />
     <Footer />
   </div>
 );
