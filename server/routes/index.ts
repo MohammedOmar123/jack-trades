ceab551eed2ab217c4d9832e80f0dc8f28bc9446
unknown <saritadh74@gmail.com>
2022-10-24 14:35:36 +0300

    changed th position of the authentication middleware relates #17

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,13 +7,11 @@ import productsRouter from './products';
 import requestRouter from './request';
 import notificationRouter from './notifications';
 
-import { authentication } from '../middlewares';
-
 const router = express.Router();
 router.use('/account', accountRouter);
 router.use('/website', websiteRouter);
 router.use('/user', userRouter);
-router.use('/products', authentication, productsRouter);
+router.use('/products', productsRouter);
 router.use('/requests', requestRouter);
 router.use('/notifications', notificationRouter);
 export default router;
