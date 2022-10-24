b6161cfe9bc1b5beaa5ba879dd82e140c1ee0a0b
unknown <saritadh74@gmail.com>
2022-10-24 16:59:02 +0300

    modified the update data controller relates #18

--- server/controllers/products/updateProduct.ts
+++ server/controllers/products/updateProduct.ts
@@ -1,12 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { updateProductQuery } from '../../database/queries';
+import { validateUpdateData } from '../../validation';
 
 const updateProduct = async (req : Request, res : Response, next : NextFunction) => {
-  const { productId } = req.params;
-  const { title, description } = req.body;
-
   try {
-    await updateProductQuery({ id: productId, title, description });
+    const data = await validateUpdateData.validateAsync({
+      id: +req.params.productId, ...req.body,
+    });
+
+    console.log(data);
+
+    await updateProductQuery(data);
     res.status(201).send({ message: 'You updated your product successfully' });
   } catch (error) {
     next(error);
