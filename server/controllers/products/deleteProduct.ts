727bab1264135c6a793493464a390d552cfe06f1
unknown <saritadh74@gmail.com>
2022-10-24 12:20:49 +0300

    finished the controller which deletes a produc relates #17

--- server/controllers/products/deleteProduct.ts
+++ server/controllers/products/deleteProduct.ts
@@ -1,7 +1,14 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { deleteProductQuery } from '../../database/queries';
 
-const deleteProduct = (req : Request, res : Response) => {
-  res.send('Hello from add delete Product');
+const deleteProduct = async (req : Request, res : Response, next:NextFunction) => {
+  const { productId } = req.params;
+  try {
+    await deleteProductQuery(productId);
+    res.send({ message: 'Product deleted successfully' });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default deleteProduct;
