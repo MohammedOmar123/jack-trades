64f03af0059a66a7ee64618c3787cd235de170c9
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2022-10-25 12:21:03 +0300

    Fixes

--- server/controllers/account/signIn.ts
+++ server/controllers/account/signIn.ts
@@ -1,24 +1,25 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import { compare } from 'bcrypt';
 import { signinValidationSchema } from '../../validation';
 import { signinQuery } from '../../database/queries';
 import { CustomError } from '../../helpers';
 
-const signIn = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+const signIn = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = await signinValidationSchema(req.body);
 
-  const validationResponse = await signinValidationSchema({ email, password });
+    const userData = await signinQuery({ email });
+    if (!userData) throw new CustomError(404, 'you have to signup first');
 
-  const userData = await signinQuery({ email });
-  if (!userData) throw new CustomError(404, 'you have to signup first');
+    const isPasswordTrue = await compare(password, password);
 
-  const isPasswordTrue = await compare(password, userData.toJSON().password);
+    if (!isPasswordTrue) throw new CustomError(404, 'You enterd a wrong password');
 
-  if (!isPasswordTrue) throw new CustomError(404, 'You enterd a wrong password');
-
-  res.send({ message: 'logged successfully' });
+    res.send({ message: 'logged successfully' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default signIn;
--- server/validation/signinValidation.ts
+++ server/validation/signinValidation.ts
@@ -1,23 +1,23 @@
 import Joi from 'joi';
-import { CustomError } from '../helpers';
 
-const signinValidationSchema = ({ email, password }) => {
+const signinValidationSchema = ({
+  email, password,
+}) => {
   const schema = Joi.object({
-    email: Joi.string().email({ minDomainSegments: 2 }).required().min(6),
-    password: Joi.string().pattern(/^(?!.*[\s])(?=.*[0-9])(?=.*\W)[a-zA-Z0-9\W]{6,15}$/).required()
-      .messages({
-        'string.empty': '"a" cannot be an empty field',
-        'string.min': '"a" should have a minimum length of 6',
-        'any.required': '"a" is a required field',
-        'string.pattern.base': 'Password must be at least 6 characters from letters,digits and special characters',
-      }),
+
+    email: Joi.string().email().required().messages({
+      'any.required': 'Email is required',
+      'string.empty': 'Email is required',
+      'string.email': 'Invalid Email',
+    }),
+    password: Joi.string().pattern(/^(?!.*[\s])(?=.*[0-9])(?=.*\W)[a-zA-Z0-9\W]{6,15}$/).required().messages({
+      'any.required': 'Password is required',
+      'string.empty': 'Password is required',
+      'string.pattern.base': 'Password must be at least 6 characters, and contain letters, digits and special characters only.',
+    }),
   });
-
-  try {
-    return schema.validateAsync({ email, password });
-  } catch (error) {
-    throw new CustomError(400, 'validation error in signin');
-  }
+  return schema.validateAsync({
+    email, password,
+  });
 };
-
 export default signinValidationSchema;
