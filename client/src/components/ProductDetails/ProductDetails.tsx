bd80592bba55c26c2aaf7c48fc9f0c9767c7c181
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-26 13:11:02 +0300

    I created three functions to handle wish list feature, checkWishList function to check if the item is already exsit in the wish list addToWishList to send post request to add new item to wish list, deleteFromWishList to send delete request to  delete item from the wishlist relates 104

--- client/src/components/ProductDetails/ProductDetails.tsx
+++ client/src/components/ProductDetails/ProductDetails.tsx
@@ -1,5 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import ButtonComponent from '../Button/Button';
 import { IProductDetailsProps } from '../../interfaces';
 import './ProductDetails.css';
@@ -10,15 +13,68 @@ const ProductDetailsComponent = ({
 }
 : IProductDetailsProps) => {
   const [FavIcon, setFavIcon] = useState('FavoriteBorder');
-  const handleIsFav = () => {
-    setFavIcon(FavIcon === 'FavoriteBorder' ? 'Favorite' : 'FavoriteBorder');
+  const { id } = useParams();
+
+  const checkWishList = async () => {
+    const response = await axios.get(`/api/v1/requests/checkFavReq/${id}`);
+    if (response.data === true) {
+      setFavIcon('Favorite');
+    }
   };
+
+  useEffect(() => {
+    checkWishList();
+  }, []);
+  const handleIsFav = () => {
+    const addToWishList = async () => {
+      try {
+        const res = await axios.post(`/api/v1/wishlist/${id}`);
+        Swal.fire(
+          'Added successfully',
+          res.data,
+          'success',
+        );
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.response.data,
+        });
+      }
+    };
+
+    const deleteFromWishList = async () => {
+      try {
+        const res = await axios.delete(`/api/v1/wishlist/${id}`);
+        Swal.fire(
+          'deleted successfully',
+          res.data,
+          'success',
+        );
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.response.data,
+        });
+      }
+    };
+
+    setFavIcon(FavIcon === 'FavoriteBorder' ? 'Favorite' : 'FavoriteBorder');
+    if (FavIcon === 'FavoriteBorder') {
+      addToWishList();
+    } else {
+      deleteFromWishList();
+    }
+  };
+
   const handleRequest = () => {
     console.log('Hello handle Request !');
   };
   const handleContactSeller = () => {
     console.log('Hello contact seller');
   };
+
   const convertDate = (timeStamp:string) => {
     const date = new Date(timeStamp);
     return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
