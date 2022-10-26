651d4b1cfe7de405e2a4fe7fcd49ff075d4110be
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-10-26 13:43:17 +0300

    created index file for all wishList controllers relates #104

---
+++ server/controllers/wishList/index.ts
@@ -0,0 +1,5 @@
+import getAllWishListItems from './getAllWishlistItems';
+import addToWishList from './addToWishList';
+import deleteFromWishList from './deleletFromWishList';
+
+export { getAllWishListItems, addToWishList, deleteFromWishList };
