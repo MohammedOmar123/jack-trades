df62f1a3511c31d5191207d069cc13cd7306ecc7
unknown <saritadh74@gmail.com>
2022-10-26 15:11:45 +0300

    modified update and delete controllers and queries relates #18

--- server/controllers/products/updateProduct.ts
+++ server/controllers/products/updateProduct.ts
@@ -1,17 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import { updateProductQuery } from '../../database/queries';
 import { validateUpdateData } from '../../validation';
 import { CustomError } from '../../helpers';
+import { IRequestPayload } from '../../interfaces/IRequestPayload';
 
-const updateProduct = async (req: Request, res: Response, next: NextFunction) => {
+const updateProduct = async (req: IRequestPayload, res: Response, next: NextFunction) => {
   try {
     const data = await validateUpdateData.validateAsync({
       id: +req.params.productId, ...req.body,
     });
 
-    const [updated] = await updateProductQuery(data);
+    const userId = req.user.id;
+    const [updated] = await updateProductQuery(data, userId);
 
-    if (updated) res.status(200).send({ message: 'You updated your product successfully' });
+    if (updated) res.json({ message: 'You updated your product successfully' });
     else throw new CustomError(400, 'Bad Request, id doesn\'t exist');
   } catch (error) {
     next(error);
--- server/database/queries/products/deleteProductQuery.ts
+++ server/database/queries/products/deleteProductQuery.ts
@@ -1,8 +1,9 @@
 import { Product } from '../../../models';
 
-const deleteProductQuery = async (id:string) => Product.destroy({
+const deleteProductQuery = async (id: string, user_id: number) => Product.destroy({
   where: {
     id,
+    user_id,
     deletedAt: null,
   },
 });
