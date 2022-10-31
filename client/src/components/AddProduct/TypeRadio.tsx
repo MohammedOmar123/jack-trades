c1e7cdbb8e1fe4262128acad95ed5dfe527ac728
unknown <saritadh74@gmail.com>
2022-10-31 11:47:36 +0200

    added states and fetch requests in client side relates #15

--- client/src/components/AddProduct/TypeRadio.tsx
+++ client/src/components/AddProduct/TypeRadio.tsx
@@ -1,28 +1,36 @@
-import { FC } from 'react';
+import { FC, ChangeEvent } from 'react';
 import {
   Radio, RadioGroup, FormControlLabel,
   FormControl, FormLabel,
 } from '@mui/material';
+import { ITypeRadioProps } from '../../interfaces';
 
-const TypeRadio:FC = () => (
-  <FormControl className="type">
-    <FormLabel id="demo-row-radio-buttons-group-label">Type</FormLabel>
-    <RadioGroup
-      row
-      aria-labelledby="demo-row-radio-buttons-group-label"
-      name="row-radio-buttons-group"
-    >
-      <FormControlLabel
-        value="Donation"
-        control={<Radio />}
-        label="Donation"
-      />
-      <FormControlLabel
-        value="Exchange"
-        control={<Radio />}
-        label="Exchange"
-      />
-    </RadioGroup>
-  </FormControl>
-);
+const TypeRadio:FC<ITypeRadioProps> = ({ setType }) => {
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setType(e.target.value);
+  };
+
+  return (
+    <FormControl className="type">
+      <FormLabel id="demo-row-radio-buttons-group-label">Type</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+        onChange={handleChange}
+      >
+        <FormControlLabel
+          value="donation"
+          control={<Radio />}
+          label="Donation"
+        />
+        <FormControlLabel
+          value="exchange"
+          control={<Radio />}
+          label="Exchange"
+        />
+      </RadioGroup>
+    </FormControl>
+  );
+};
 export default TypeRadio;
--- client/src/components/AddProduct/index.tsx
+++ client/src/components/AddProduct/index.tsx
@@ -1,90 +1,172 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { FC } from 'react';
 import {
-  TextField,
-  IconButton,
-  Box,
-  Typography,
-  Button,
+  FC, ChangeEvent, useState, FormEvent,
+} from 'react';
+import {
+  TextField, IconButton, Box, Typography, Button,
 } from '@mui/material';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import './AddProduct.css';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import TypeRadio from './TypeRadio';
 import CategoryRadio from './CategoryRadio';
+import { validateProduct } from '../../validation';
 
-const AddProduct:FC = () => (
-  <form className="newProduct">
-    <fieldset>
-      <legend>Add new product:</legend>
-      <TextField
-        id="outlined-text"
-        label="Title"
-        className="info"
-        type="text"
-      />
+const AddProduct:FC = () => {
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [gallery, setGallery] = useState<(string | File)[]>([]);
+  const [type, setType] = useState<string>('');
+  const [category, setCategory] = useState<string>('');
+  const [errMessage, setErrMessage] = useState<string>('');
 
-      <TextField
-        id="outlined-textarea"
-        label="Description"
-        placeholder="description"
-        minRows={3}
-        maxRows={6}
-        className="info"
-        multiline
-      />
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === 'title') setTitle(e.target.value);
+    else setDescription(e.target.value);
+  };
 
-      <TypeRadio />
-      <CategoryRadio />
+  const handleImage = async (e:ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      try {
+        const data = new FormData();
+        data.append('file', e.target.files[0]);
+        data.append('upload_preset', 'mohammed');
+        data.append('cloud_name', 'dginxclgp');
+        const response = await axios
+          .post('https://api.cloudinary.com/v1_1/dginxclgp/image/upload', data);
 
-      <Typography variant="h5">Choose Images</Typography>
-      <Box className="images">
-        <Box className="upload-img">
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="label"
+        setGallery((prev) => [...prev, response.data.url]);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleSubmit = async (e:FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const data = await validateProduct.validate({
+        title, description, type, gallery, category_id: +category,
+      });
+
+      const response = await axios.post('/api/v1/products', data);
+      await Swal.fire({
+        title: response.data.message,
+        confirmButtonColor: '#1b4b66',
+      });
+    } catch (error) {
+      if (error.name === 'ValidationError') {
+        await Swal.fire({
+          title: error.message,
+          confirmButtonColor: '#1b4b66',
+        });
+      }
+    }
+  };
+
+  return (
+    <form
+      className="newProduct"
+      onSubmit={handleSubmit}
+    >
+      <fieldset>
+        <legend>Add new product:</legend>
+        <TextField
+          id="outlined-text"
+          label="Title"
+          name="title"
+          className="info"
+          type="text"
+          onChange={handleChange}
+          value={title}
+        />
+
+        <TextField
+          id="outlined-textarea"
+          label="Description"
+          name="description"
+          placeholder="description"
+          minRows={3}
+          maxRows={6}
+          className="info"
+          onChange={handleChange}
+          value={description}
+          multiline
+        />
+
+        <TypeRadio setType={setType} />
+        <CategoryRadio setCategory={setCategory} />
+
+        <Typography variant="h5">Choose Images</Typography>
+        <Box className="images">
+          <Box
+            className="upload-img"
+            sx={{
+              backgroundImage: `url(${gallery[0]})`,
+            }}
           >
-            <input hidden accept="image/*" type="file" />
-            <PhotoCamera />
-          </IconButton>
-        </Box>
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="label"
+            >
+              <input
+                hidden
+                accept="image/*"
+                type="file"
+                onChange={handleImage}
+              />
+              <PhotoCamera />
+            </IconButton>
+          </Box>
 
-        <Box className="upload-img">
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="label"
+          <Box
+            className="upload-img"
+            sx={{
+              backgroundImage: `url(${gallery[1]})`,
+            }}
           >
-            <input hidden accept="image/*" type="file" />
-            <PhotoCamera />
-          </IconButton>
-        </Box>
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="label"
+            >
+              <input
+                hidden
+                accept="image/*"
+                type="file"
+                onChange={handleImage}
+              />
+              <PhotoCamera />
+            </IconButton>
+          </Box>
 
-        <Box className="upload-img">
-          <IconButton
-            color="primary"
-            aria-label="upload picture"
-            component="label"
+          <Box
+            className="upload-img"
+            sx={{
+              backgroundImage: `url(${gallery[2]})`,
+            }}
           >
-            <input hidden accept="image/*" type="file" />
-            <PhotoCamera />
-          </IconButton>
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="label"
+            >
+              <input
+                hidden
+                accept="image/*"
+                type="file"
+                onChange={handleImage}
+              />
+              <PhotoCamera />
+            </IconButton>
+          </Box>
         </Box>
-      </Box>
-      <Button variant="contained" type="submit">Submit</Button>
-    </fieldset>
-  </form>
-);
+        <Button variant="contained" type="submit">Submit</Button>
+      </fieldset>
+    </form>
+  );
+};
 
 export default AddProduct;
-
-// <Button
-//   variant="contained"
-//   component="label"
-// >
-//   Upload File
-//   <input
-//     type="file"
-//     hidden
-//   />
-// </Button>
