9a97717d87c565fa1446bdb50d0221e329d5dd21
unknown <saritadh74@gmail.com>
2022-11-03 12:23:22 +0200

    added new states and handled the requestingan item request relates #117

--- client/src/components/Context/ImageContext.tsx
+++ client/src/components/Context/ImageContext.tsx
@@ -1,10 +1,18 @@
 import React, { createContext, useState } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import { IImageContext, IProviderProps } from '../../interfaces';
 
 export const ImageContext = createContext<IImageContext>({
   setMainImage: () => {},
   mainImage: '',
   gallery: [''],
+  setProductArray: () => {},
+  productArray: [1],
+  setProductId: () => {},
+  handleRequest: () => {},
+  setOpen: () => {},
+  open: false,
 });
 
 export const ImageContextProvider: React.FC<IProviderProps> = ({
@@ -12,10 +20,50 @@ export const ImageContextProvider: React.FC<IProviderProps> = ({
   gallery,
 }: IProviderProps) => {
   const [mainImage, setMainImage] = useState(gallery[0]);
+  const [productArray, setProductArray] = useState<number[]>([]);
+  const [productId, setProductId] = useState<number>(0);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleRequest = async () => {
+    try {
+      const alert = await Swal.fire({
+        title: 'Confirm request?',
+        text: "You won't be able to revert this!",
+        showCancelButton: true,
+        confirmButtonColor: '#1b4b66',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+      });
+      if (alert.isConfirmed) {
+        const response = await axios.post('/api/v1/requests', {
+          productId,
+          products: productArray || null,
+        });
+        const res = await Swal.fire(response.data.message);
+        if (res.isConfirmed) {
+          setProductArray([]);
+          setOpen(false);
+        }
+      }
+    } catch (error) {
+      await Swal.fire(error.response.data.message);
+    }
+  };
 
   return (
   // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <ImageContext.Provider value={{ mainImage, setMainImage, gallery }}>
+    <ImageContext.Provider value={{
+      mainImage,
+      setMainImage,
+      gallery,
+      setProductArray,
+      productArray,
+      setProductId,
+      handleRequest,
+      open,
+      setOpen,
+    }}
+    >
       {children}
     </ImageContext.Provider>
   );
