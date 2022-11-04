40845a4b3ea62c45f720625a69d6468722b12135
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-11-04 02:40:54 +0200

    handled updated request and all  his cases relates #123

--- server/controllers/request/updateRequest.ts
+++ server/controllers/request/updateRequest.ts
@@ -1,7 +1,45 @@
-import { Request, Response } from 'express';
+import { Response, NextFunction } from 'express';
+import { IRequestPayload } from '../../interfaces';
+import {
+  updateRequestQuery, getIsExchangeableQuery,
+  declineAllOtherRequests, checkRequestQuery,
+  deleteSuccessRequestQuery,
+} from '../../database/queries';
+import { updateRequestValidation } from '../../validation';
+import { CustomError } from '../../helpers';
 
-const updateRequest = (req : Request, res : Response) => {
-  res.send('Hello from updateRequest');
+const updateRequest = async (req: IRequestPayload, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.user;
+    const { requestId } = req.params;
+    const { receiverApproval, productId } = req.body;
+
+    if (!(Number(requestId) > 0)) throw new CustomError(401, 'Bad Request');
+    await updateRequestValidation.validateAsync({ receiverApproval });
+
+    // check if the request still valid and for the current user
+    const queryResult = await checkRequestQuery(+requestId, +id);
+    if (!queryResult) throw new CustomError(400, 'This request does\'nt exist anymore');
+    if (!receiverApproval) {
+      await updateRequestQuery(+requestId, false, null, 'fail');
+      res.json({ message: 'You decline the request' });
+      return;
+    }
+
+    const queryOutput = await getIsExchangeableQuery(+requestId);
+    if (!queryOutput.is_exchangable) {
+      updateRequestQuery(+requestId, true, null, 'success');
+    } else {
+      if (!queryResult.products.includes(productId)) throw new CustomError(400, 'Choose one of the offered products');
+      await updateRequestQuery(+requestId, true, productId, 'success');
+    }
+
+    await declineAllOtherRequests(+requestId, (+queryOutput.product_id));
+    await deleteSuccessRequestQuery(+requestId, id);
+    res.json({ message: 'Operation done successfully' });
+  } catch (error) {
+    next(error);
+  }
 };
-// Review function name and file name Here !
+
 export default updateRequest;
