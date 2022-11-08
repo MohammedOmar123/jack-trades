647b9d09c16280d6b6d0e305681f17fef5165348
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-11-08 13:17:18 +0200

    created add message and get all messages queries relates #134

---
+++ server/database/queries/chat/getAllMessageQuery.ts
@@ -0,0 +1,35 @@
+import { Op } from 'sequelize';
+import { Chat } from '../../../models';
+import User from '../../../models/users';
+
+const getAllMessagesQuery = (
+  sender_id:number,
+  receiver_id:number,
+) => Chat.findAll({
+  attributes: ['message', 'sender_id', 'receiver_id'],
+  raw: true,
+  nest: false,
+  where: {
+    [Op.or]: [{
+      sender_id,
+      receiver_id,
+    },
+    {
+      sender_id: receiver_id,
+      receiver_id: sender_id,
+    }],
+  },
+  include: [{
+    model: User,
+    attributes: ['first_name', 'image'],
+    as: 'sender',
+  },
+  {
+    model: User,
+    attributes: ['first_name', 'image'],
+    as: 'receiver',
+  },
+  ],
+});
+
+export default getAllMessagesQuery;
