5f6bb4a6bcbe35c841c9790e33e28aaa1b144b4c
unknown <saritadh74@gmail.com>
2022-11-15 20:52:24 +0200

    modified auth route, and fixed the z-index of the swal alert

--- server/routes/account.ts
+++ server/routes/account.ts
@@ -2,10 +2,9 @@ import express from 'express';
 import {
   signIn, signup, signOut, checkAuth,
 } from '../controllers';
-import { authentication } from '../middlewares';
 
 const router = express.Router();
-router.get('/', authentication, checkAuth);
+router.get('/', checkAuth);
 router.post('/signup', signup);
 router.post('/signin', signIn);
 router.post('/logout', signOut);
