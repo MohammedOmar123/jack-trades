c28116c89677e7351d5cc504f2fbd56e4a754cc1
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-11-16 05:54:31 +0200

    updated backend controller relates #177

--- server/database/queries/user/getUserProfile.ts
+++ server/database/queries/user/getUserProfile.ts
@@ -2,7 +2,7 @@ import { User } from '../../../models';
 
 const getUserProfileQuery = (id: number) => User.findOne({
   where: { id },
-  attributes: ['first_name', 'last_name', 'image', 'bio'],
+  attributes: ['first_name', 'last_name', 'image', 'bio', 'email'],
 });
 
 export default getUserProfileQuery;
--- server/validation/updateUserProfileValidation.ts
+++ server/validation/updateUserProfileValidation.ts
@@ -1,9 +1,16 @@
 import Joi from 'joi';
 
 const updateUserProfileValidation = Joi.object({
-  bio: Joi.string(),
-  image: Joi.string().pattern(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpeg|jpg|gif|png)/i).messages({
-    'string.pattern.base': 'Add a valid URL and only these extensions [jpeg,jpg,git,png] are allowed ',
+  email: Joi.string().email().required(),
+  first_name: Joi.string().pattern(/^[a-zA-z]{2,20}$/).required().messages({
+    'string.pattern.base': 'First name must be 2-20 characters long, and contain only letters.',
+  }),
+  last_name: Joi.string().pattern(/^[a-zA-z]{2,20}$/).required().messages({
+    'string.pattern.base': 'Last name must be 2-20 characters long, and contain only letters.',
+  }),
+  bio: Joi.string().allow(''),
+  image: Joi.string().allow(null).pattern(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpeg|jpg|gif|png)/i).messages({
+    'string.pattern.base': 'Please add a valid URL and only these extensions [jpeg,jpg,git,png] are allowed',
   }),
 }).or('bio', 'image');
 
